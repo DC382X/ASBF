7ac4b96de72a8d07352b919aade67b725a211919
DC382X <82519449+DC382X@users.noreply.github.com>
2021-05-04 17:23:49 -0500

    Create bookmarklet.js

---
+++ bookmarklet.js
@@ -0,0 +1,5 @@
+var r=prompt("Message")
+var z=prompt("Name");
+setTimeout(function(){
+var e=window.open()
+e.document.write('<style>body { background:#fff; font-family:Arial; margin:0; padding:0; }</style><title>Tech</title><h1><center><b><a href="javascript:var z=window.open();z.document.write(&apos;'+r+'&apos;)"> '+z+'</a></b></center></h1>\nBase Code: <input value="javascript:var z=window.open();z.document.write(&apos;'+r+'&apos;)" size="152" readonly>\nDrag Any To Bookmarks Bar to have for the future.')},100);
